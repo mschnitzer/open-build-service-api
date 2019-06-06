396cce230df3a2c4882eaea47ac4c30a11a5df2b
Manuel Schnitzer <webmaster@mschnitzer.de>
2019-06-06 20:29:26 +0200

    Use environment variables for username and password in examples

--- examples/simple_authentication.rb
+++ examples/simple_authentication.rb
@@ -1,10 +1,7 @@
 #!/usr/bin/ruby
 require_relative '../lib/open-build-service-api'
 
-username = '' # username of your OBS account
-password = '' # password of your OBS account
-
-obs_api = OpenBuildServiceAPI::Connection.new(username, password)
+obs_api = OpenBuildServiceAPI::Connection.new(ENV['OBS_USER'], ENV['OBS_PASS'])
 
 puts "successfully authenticated"
 puts "Last deployment: #{obs_api.about.last_deployment}"
