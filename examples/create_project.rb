7c9dc4ba5d73a01e6a8e0d6a0baf79087277005e
Manuel Schnitzer <webmaster@mschnitzer.de>
2019-06-09 14:34:09 +0200

    Check for project name parameter in example
    This script can only be called with an argument.

--- examples/create_project.rb
+++ examples/create_project.rb
@@ -1,6 +1,11 @@
 #!/usr/bin/ruby
 require_relative '../lib/open-build-service-api'
 
+if ARGV.empty?
+  $stderr.puts 'please provide a project name.'
+  exit! 1
+end
+
 obs_api = OpenBuildServiceAPI::Connection.new(ENV['OBS_USER'], ENV['OBS_PASS'])
 
 t = obs_api.projects.create(ARGV[0])
