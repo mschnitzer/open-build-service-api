d334dd8cfceb224e4b76ec96ef9566530620de0e
Manuel Schnitzer <webmaster@mschnitzer.de>
2019-06-09 14:24:54 +0200

    Extract information from API response
    The build service responds with some error details if an API call
    failed. We should extract and store that information.

--- lib/exception.rb
+++ lib/exception.rb
@@ -1,11 +1,23 @@
 module OpenBuildServiceAPI
   # remote API network errors
   class RemoteAPIError < Exception
-    attr_accessor :response, :message
+    attr_accessor :response, :message, :error_code, :error_summary
 
     def initialize(response, message=nil)
+      response_body = response.body
+
       @response = response
-      @message = message ? message : response.body
+      @message = message ? message : response_body
+
+      response_xml = Nokogiri::XML(response_body)
+      status = response_xml.xpath('./status')[0]
+
+      if status
+        @error_code = status.attr('code')
+
+        summary = status.xpath('./summary')
+        @error_summary = summary.text if summary[0]
+      end
     end
 
     def to_s
@@ -16,10 +28,13 @@ module OpenBuildServiceAPI
   class InternalServerError < RemoteAPIError; end
   class AuthenticationError < RemoteAPIError; end
   class NotFoundError < RemoteAPIError; end
+  class PermissionDeniedError < RemoteAPIError; end
 
   # remote API errors
   class APIError < Exception; end
   class ProjectNotFoundError < APIError; end
+  class ProjectCreationPermissionError < APIError; end
+  class ProjectCreationFailedError < APIError; end
 
   # library specific exceptions
   class GeneralError < Exception; end
